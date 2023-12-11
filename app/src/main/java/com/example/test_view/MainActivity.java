9e84326927ef4e3b822ccf1372ccf343df0eaaad
Yuto <Yuuto.ba20050226@iCloud.com>
2023-12-11 23:42:36 +0900

    ADD : Many Buttons in ScrollView ADD : Menu in upper right

--- app/src/main/java/com/example/test_view/MainActivity.java
+++ app/src/main/java/com/example/test_view/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.test_view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
         view.setConText(context_main);
         Init();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.title_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()) {
+//            R.id.action_home ->
+//            R.id.action_user ->
+//            R.id.action_map ->
+//        }
+
+        view.menuMethod(item);
+//        String message = "「" + item.getTitle() + "」が押されました。";
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
 
     protected void Init(){
         //viewの初期化
--- app/src/main/java/com/example/test_view/View.java
+++ app/src/main/java/com/example/test_view/View.java
@@ -1,27 +1,39 @@
 package com.example.test_view;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-
 import android.content.Context;
+import android.graphics.Color;
 import android.text.Editable;
-import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.widget.AdapterView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.internal.TextWatcherAdapter;
-import com.google.android.material.navigation.NavigationBarView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+// TODO
+/*
+ * ボタンの動的生成
+ * Spinnerで選択した品詞を受け取ってボタンの変化
+ * 動的に生成したボタンを押したときにどうやって送る文字列を特定するか
+ * 登録する文字列の管理　DB?strings.xmlに直接書き込む?
+ * 色々な解釈をする単語への対応
+ * 画面遷移
+ */
 public class View extends AppCompatActivity implements TextWatcher {
+
+
 
     TextView main_title;
     Button Change_mode;
@@ -35,6 +47,12 @@ public class View extends AppCompatActivity implements TextWatcher {
 
     Spinner types;
 
+    android.view.View layout;
+
+    ScrollView scrollView;
+
+    public Button button1;
+
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
@@ -47,18 +65,18 @@ public class View extends AppCompatActivity implements TextWatcher {
 
 
     // Viewのコンストラクタ
-    protected View(){
+    protected View() {
         Log.d("View", "View");
     }
 
     //contextをセットするメソッド　static忘れない
-    public static void setConText(Context ct){
+    public static void setConText(Context ct) {
         conText_main = ct;
         Log.d("View", "set_Contest");
     }
 
     // Viewの初期化 main_titleの初期化を行っている
-    public void init(){
+    public void init() {
         Log.d("View", "init");
 //        fm.beginTransaction().replace(R.id.text_text_UI, new Text_Text()).commit();
 
@@ -71,6 +89,24 @@ public class View extends AppCompatActivity implements TextWatcher {
         Input_Text.addTextChangedListener(this);
         OutPut_Text = ((com.example.test_view.MainActivity) conText_main).findViewById(R.id.Output_Text);
         types = (Spinner) ((com.example.test_view.MainActivity) conText_main).findViewById(R.id.people);
+//        layout = ((com.example.test_view.MainActivity) conText_main).findViewById(R.id.Select_Buttons);
+//        scrollView = ((com.example.test_view.MainActivity) conText_main).findViewById(R.id.Buttons);
+        //scrollviewの設定
+//        scrollView =((com.example.test_view.MainActivity) conText_main).findViewById(R.id.Buttons);
+//        scrollView.setTranslationY(500);
+//        setContentView(R.layout.activity_main);
+//        addContentView(scrollView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+//        LinearLayout linearLayout = new LinearLayout(conText_main);
+//        linearLayout.setOrientation(LinearLayout.VERTICAL);
+//        scrollView.addView(linearLayout);
+//        button1 = new Button(this);
+//        button1.setTextSize(50);
+//        button1.setText("A");
+//        button1.setTag(1);
+//        linearLayout.addView(Start_trans);
+
+//        linearLayout.setBackgroundColor(Color.rgb(0, 0, 0));
 
         //タイトルにタイトルをセット
         main_title.setText("Santa Claus Japanese Translate");
@@ -82,8 +118,7 @@ public class View extends AppCompatActivity implements TextWatcher {
         Change_lang.setOnClickListener(listener);
 
         //品詞選択spinnerの文字列を取得
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(conText_main,
-                R.array.people_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(conText_main, R.array.people_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
@@ -93,9 +128,17 @@ public class View extends AppCompatActivity implements TextWatcher {
         onItemSelectListener onItemSelectListener = new onItemSelectListener(conText_main);
         types.setOnItemSelectedListener(onItemSelectListener);
 
+        //Layoutの設定
 
     }
+    public void menuMethod(@NonNull MenuItem item){
 
+        String message = "「" + item.getTitle() + "」が押されました。";
+        Toast.makeText(conText_main, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    //これ以降は一応使わない
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -122,6 +165,7 @@ public class View extends AppCompatActivity implements TextWatcher {
 //            Log.d("View", "onClick Trans_Start");
 //        }
 //    }
+
 }
 
 //buttonSetting.setOnClickListener(new View.OnClickListener() {
